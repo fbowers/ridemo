979eba611369ff9a4abce3dac608339c9ada79eb
fbowers <fbowers@pestnow.com>
2011-10-27 15:04:48 -0400

    -

---
+++ tiny/Gemfile
@@ -0,0 +1,6 @@
+source 'http://rubygems.org'
+
+gem 'rails'
+gem 'tiny_tds'
+gem 'activerecord-sqlserver-adapter'
+
---
+++ tiny/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tiny::Application.config.secret_token = '3437b4ae6c74a343e31b11655c700fc802cda76398af90d7553a5e73a11808618dd5f91c30984f0da09b762f11b39e3d88596488720be2489bddf95db3ffa934'
